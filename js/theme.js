5af2a7a41d9f854c8c9e0849d330cf2a39445e4b
Ethic Mthobisi Hadebe <ethichadebe@gmail.com>
2020-08-05 13:53:44 +0200

    Updates

---
+++ js/theme.js
@@ -0,0 +1,8 @@
+//Change icon arccording to browser theme
+var icon = document.getElementById('icon'); //or grab it by tagname etc
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    icon.href = "images/WhiteLogo.png";
+} else {
+    icon.href = "images/BlackLogo.png";
+}
