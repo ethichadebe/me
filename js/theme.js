dc6fc1bf4bee50e1ceca0752289d7775c9fb12a6
Ethic Mthobisi Hadebe <ethichadebe@gmail.com>
2020-08-06 18:58:41 +0200

    updates

--- js/theme.js
+++ js/theme.js
@@ -2,7 +2,7 @@
 var icon = document.getElementById('icon'); //or grab it by tagname etc
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    icon.href = "images/WhiteLogo.png";
+    icon.href = "images/WhiteLogo.webp";
 } else {
-    icon.href = "images/BlackLogo.png";
+    icon.href = "images/BlackLogo.webp";
 }
