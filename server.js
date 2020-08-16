832a0d36f026e3dd81c70ec368d896b227c278ea
Ethic Mthobisi Hadebe <ethichadebe@gmail.com>
2020-08-16 12:55:48 +0200

    change host

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+//Loads static stuff
+app.use(express.static(__dirname));
+
+//routes
+app.get("/", function(req, res) {
+    res.render("index");
+})
+
+app.listen(port, function() {
+    console.log("works")
+})
